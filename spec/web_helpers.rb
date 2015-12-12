33296c85970ae004a93e53d80f9619542acf9365
hibreez <4ngus.whitehead@gmail.com>
2015-12-12 19:29:55 +0000

    port change

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,6 +1,6 @@
 def sign_in_and_play(player1, player2) 
   allow(Kernel).to receive(:rand).and_return 0
-  visit 'http://localhost:9292/'
+  visit 'http://localhost:1337/'
   fill_in :name1, with: player1
   fill_in :name2, with: player2
   click_button 'FIGHT!'
@@ -8,7 +8,7 @@ end
 
 def sign_in_and_play_ai(player1, player2)
   allow(Kernel).to receive(:rand).and_return 0
-  visit 'http://localhost:9292/'
+  visit 'http://localhost:1337/'
   fill_in :name1, with: player1
   fill_in :name2, with: player2
   check 'ai'
